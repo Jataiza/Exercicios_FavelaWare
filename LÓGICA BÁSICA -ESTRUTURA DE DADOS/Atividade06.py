a91280f368d6e0f889626793a2b23dc96526eb26
Jataiza <85134137+Jataiza@users.noreply.github.com>
2022-07-26 15:08:42 -0300

    Atualizando Atividade06

--- LÓGICA BÁSICA -ESTRUTURA DE DADOS/Atividade06.py
+++ LÓGICA BÁSICA -ESTRUTURA DE DADOS/Atividade06.py
@@ -3,30 +3,22 @@
 # receber esses valores e armazenar em uma lista até que o valor inserido pelo usuário seja um valor em branco (uma
 # string vazia). Converta todos os valores presentes na lista
 # para graus Celsius e imprima na tela em uma formatação amigável ao usuário.
-def menu_inicial():
-    print('Programa para Conversão de Temperaturas')
-    print('1. Converter de Celsius para Fahrenheit')
-    print('2. Converter de Fahrenheit para Celsius')
+lista= list()
+quantTemp= int(input("informe a quantidade de temperaturas a serem digitadas"))
+i=0
+graus=0.0
+ii=0
+while quantTemp > i:
+    
+    lista.append(float(input("escreva temperatura")))
+    i+=1
 
-def cel_fahr():
-    C = float(input('Entre com a temperatura em graus Celsius: '))
-    F = C * (9 / 5) + 32
-    print('Valor em Fahrenheit: {0}°F'.format(F))
 
-def fahr_cel():
-    F = float(input('Entre com a temperatura em graus Fahrenheit: '))
-    C = (F - 32) * (5 / 9)
-    print('Valor em Celsius: {0}°C'.format(C))
-
-if __name__=='__main__':
-    menu_inicial()
-    escolha = input('Escolha o tipo de conversão que deseja realizar: ')
-
-    if escolha == '1':
-        cel_fahr()
-
-    if escolha == '2':
-        fahr_cel()
-
+else:
+    print(lista)
+while lista != ii:
+    graus =(lista[ii]-32)* (5/9)
+    print (graus)
+    ii+=1
 
 
